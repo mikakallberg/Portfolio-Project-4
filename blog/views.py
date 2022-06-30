3f75b8441980b252cdfe639992a6ad20ae482a89
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-30 13:17:21 +0000

    Add html and css files

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import BlogPost
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """ Settings for front view of blog """
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
