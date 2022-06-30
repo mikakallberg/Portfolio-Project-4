20d3bed2e12228cae1b538634a88756da9c49cbb
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-30 09:05:50 +0000

    Resolved problems raised by terminal

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
+""" Admin page for settings in admin view """
 from django.contrib import admin
 from .models import BlogPost
 from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(BlogPost)
 class PostAdmin(SummernoteModelAdmin):
     """ Handle use of summernote functionality in blog """
     summernote_fields = ('blog_content')
-
