5d3bf2effd7f71f6eb8e18b2022240d6fbf18cc2
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-30 08:25:41 +0000

    Add models to comment section

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,7 @@ class BlogPost(models.Model):
     blog_content = models.TextField(default='Start your story here')
     status = models.IntegerField(choices=STATUS, default=0)
     featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField()
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
 
@@ -36,6 +37,27 @@ class BlogPost(models.Model):
 
     def number_of_likes(self):
         """
-        Handles number of likes
+        Handles total number of likes
         """
         return self.likes.count()
+
+
+class CommentSection(models.Model):
+    """ Handles comments """
+
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Handles order of published comments, oldest first.
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
