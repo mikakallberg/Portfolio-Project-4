5dd748e98ab2c8652f4239715c802cb1e3c2af63
mikakallberg <mikaela.kallberg@hotmail.com>
2022-07-07 13:44:07 +0000

    Reset database

--- blog/models.py
+++ blog/models.py
@@ -16,15 +16,15 @@ class BlogPost(models.Model):
     title = models.CharField(
         max_length=200, unique=True, null=False, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts',
-        default=id(0))
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts',
+        default=id(0))
     status = models.IntegerField(choices=STATUS, default=0)
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
 
